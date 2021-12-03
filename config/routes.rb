8b4cd8340980d4fa7af45b47e6d9560996d1230e
kazutotaniyoshi <it352032@gmail.com>
2021-12-03 12:51:21 +0000

    へだー編集&ルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  namespace :public do
+    root to: "homes#top"
+    resources :customers, only:[:show,:edit,:udate]
+      resources :request, only:[:new,:show,:create,:index]do
+        collection do
+        get 'complete'
+        end
+      end
+    end
+    namespace :admin do
+      root to: "homes#top"
+      resources :customers,only:[:index,:show,:edit,:update]
+      resources :requests, only:[:show]
+    end
 # 顧客用のログイン・サインインの設定
 # URL /customers/sign_in ...
 #スキップで余分なルーティングを非表示
