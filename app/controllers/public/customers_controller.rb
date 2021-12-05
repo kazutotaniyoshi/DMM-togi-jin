753742ff4a8c7ee44e703f342f015a5fb93c4ba8
kazutotaniyoshi <it352032@gmail.com>
2021-12-05 13:21:52 +0000

    退会メソッド導入、レイアウトはまだ

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -13,6 +13,15 @@ class Public::CustomersController < ApplicationController
     redirect_to  public_customer_path
   end
 
+  def unsubscribe
+    @customer = Customer.find_by(email: params[:email])
+  end
+  def withdraw
+    @customer = current_customer
+    @customer.update(is_valid: false)
+    reset_session
+    redirect_to root_path
+  end
   private
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :tel, :postcode, :address, :email, :password, :password_confirmation)
