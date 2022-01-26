9927a34733655c8bde01ed222773bcd9a838cb39
kazutotaniyoshi <it352032@gmail.com>
2022-01-26 10:40:14 +0000

    日付のバリデーション設定変更

--- app/models/request.rb
+++ app/models/request.rb
@@ -7,4 +7,15 @@ class Request < ApplicationRecord
    validates :time, presence: true
    validates :status, presence: true
    validates :name, presence: true
+
+   validate :date_befor_start
+    def date_befor_start
+        return if date.blank?
+    errors.add(:date, "は今日以降のものを選択してください") if date < Date.today
+    end
+
+    validate :start_time_not_thursday
+    def start_time_not_thursday
+        errors.add(:date, "は木曜日を選択できません") if date.thursday?
+    end
 end
