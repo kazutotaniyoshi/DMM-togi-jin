2e134f06f8710ae7acce86461b028c59b66a2bc7
kazutotaniyoshi <it352032@gmail.com>
2022-01-27 08:06:17 +0000

    test

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,18 +4,13 @@ class Request < ApplicationRecord
   enum status: {"しない":0,"する":1}#配送のステータス
 
    validates :date, presence: true
+    def date
+      return if date.blank?
+      errors.add(:date, "過去の日付です。") if date < Date.today
+
+      errors.add(:date, "は定休日です。") if date.thursday?
+    end
    validates :time, presence: true
    validates :status, presence: true
    validates :name, presence: true
-
-   validate :date_befor_start
-    def date_befor_start
-        return if date.blank?
-    errors.add(:date, "は今日以降のものを選択してください") if date < Date.today
-    end
-
-    validate :start_time_not_thursday
-    def start_time_not_thursday
-        errors.add(:date, "は木曜日を選択できません") if date.thursday?
-    end
 end
